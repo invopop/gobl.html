767020220fd1e4d9f9c54ee00a486694f32ed1eb
Javier Martinez <javier.martinez@invopop.com>
2025-01-17 14:48:07 +0000

    fix linter

--- layout/layout.go
+++ layout/layout.go
@@ -1,11 +1,15 @@
+// Package layout provides all the possible layouts for a document.
 package layout
 
 // Code is an ENUM representing the possible layouts.
 type Code string
 
 const (
-	A4      Code = "A4"
-	Letter  Code = "Letter"
+	// A4 is used for rendering a document with an A4 layout
+	A4 Code = "A4"
+	// Letter is used for rendering a document with an US letter layout
+	Letter Code = "Letter"
+	// DIN5008 is used for rendering a document with a german DIN5008 Form-B layout
 	DIN5008 Code = "DIN5008"
 )
 
