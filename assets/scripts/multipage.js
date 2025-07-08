3e5c4cb58cde5900b318de5107f012b80f8174d8
Luismi Cavallé <luismi@lmcavalle.com>
2025-07-08 13:39:10 +0200

    Fix multipage with large footer

--- assets/scripts/multipage.js
+++ assets/scripts/multipage.js
@@ -1,8 +1,8 @@
 Prince.trackBoxes = true;
 Prince.registerPostLayoutFunc(handleMultipage(1));
 
-const headerTempl = document.getElementById("mp-header-templ");
-const footerTempl = document.getElementById("mp-footer-templ");
+const headerTemplate = document.getElementById("mp-header-templ");
+const footerTemplate = document.getElementById("mp-footer-templ");
 
 // handleMultipage detects page breaks and attaches the header and footer templates
 // for each page with the subtotal amount
@@ -19,12 +19,12 @@ function handleMultipage(currPage) {
       const boxes = line.getPrinceBoxes();
       const page = boxes[0].pageNum
 
-      subtotal = line.getAttribute("data-subtotal");
-
       if (page > currPage) {
         handlePageBreak(currPage, subtotal);
         return;
       }
+
+      subtotal = line.getAttribute("data-subtotal");
     }
 
     const totals = document.querySelector(".tail");
@@ -40,21 +40,40 @@ function handleMultipage(currPage) {
 }
 
 function handlePageBreak(page, amount) {
-  attach(headerTempl, page + 1, amount);
-  attach(footerTempl, page, amount);
+  const headerInserted = upsertTemplate(headerTemplate, page + 1, amount);
+  const footerInserted = upsertTemplate(footerTemplate, page, amount);
 
-  // Since we have changed the DOM, we need to register a new function to handle the
-  // next page breaks after Prince refreshes the layout with the changes.
-  Prince.registerPostLayoutFunc(handleMultipage(page + 1));
+  if (headerInserted || footerInserted) {
+    // the inserted elements may have pushed lines to the next page, we need to
+    // register a new function to handle the same page again after Prince
+    // refreshes the layout with the changes
+    Prince.registerPostLayoutFunc(handleMultipage(page));
+  } else {
+    // no lines pushed, so we process the next page
+    Prince.registerPostLayoutFunc(handleMultipage(page + 1));
+  }
 }
 
-function attach(template, page, amount) {
-  var body = document.getElementsByTagName('body')[0];
-  var el = template.cloneNode(true);
-  el.id = "";
-  el.setAttribute("style", "-prince-float-defer-page: " + (page - 1));
-  el.querySelector(".amount").innerHTML = amount;
-  el.style.display = "block";
+function upsertTemplate(template, page, amount) {
+  let inserted = false;
+  const id = template.id.replace("-templ", "-" + page);
+  let el = document.getElementById(id);
 
-  body.insertBefore(el, body.firstChild);
+  if (!el) {
+    // the element doesn't exist, insert it from the template
+    el = template.cloneNode(true);
+    el.id = id;
+    el.setAttribute("style", "-prince-float-defer-page: " + (page - 1));
+    el.style.display = "block";
+
+    const body = document.getElementsByTagName('body')[0];
+    body.insertBefore(el, body.firstChild);
+    inserted = true;
+  }
+
+  // update the element based on the parameters; even if the element already
+  // existed the parameters may have changed after a DOM change
+  el.querySelector(".amount").innerHTML = amount;
+
+  return inserted;
 }
